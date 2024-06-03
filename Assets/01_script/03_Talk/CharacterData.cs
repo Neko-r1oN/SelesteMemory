d5ac7d192ebe6e4bafe449656dadd8a786086a85
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2024-06-03 16:25:52 +0900

    会話システム基盤修正

---
+++ Assets/01_script/03_Talk/CharacterData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData : MonoBehaviour
+{
+    // ------------------------------------------------------
+    // キャラクター定義.
+    // ------------------------------------------------------
+    public enum Type
+    {
+        None = 0,
+        Tona = 1,
+        Haru = 2,
+        Nanoka =3,
+        a = 4,
+        s = 5,
+        d = 6,
+        j = 7,
+        f = 8,
+        Yuki = 9
+    }
+}
